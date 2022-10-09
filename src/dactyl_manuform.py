72cc308552890b4725f4e72336619c2a31b577ab
David Gamero <david340804@gmail.com>
2022-10-08 23:58:18 -0400

    my config

--- src/dactyl_manuform.py
+++ src/dactyl_manuform.py
@@ -1189,6 +1189,7 @@ def make_dactyl():
         shape = rotate(shape, rot)
         shape = translate(shape, pos)
 
+        resin = False
         if cluster is not None and resin is False:
             shape = cluster.get_extras(shape, pos)
 
